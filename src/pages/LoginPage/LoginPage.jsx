cbb6cb54fe72dff87a571acb068988b6cdd7c3ee
Natalia-Kalashnikova <144282173+Natalia-Kalashnikova@users.noreply.github.com>
2025-01-07 23:35:12 +0200

    Add LoginPage

--- src/pages/LoginPage/LoginPage.jsx
+++ src/pages/LoginPage/LoginPage.jsx
@@ -1,5 +1,15 @@
 import css from './LoginPage.module.css';
+import RegistrationFormTitle from '../../components/Auth/RegistrationFormTitle/RegistrationFormTitle.jsx';
+import LoginForm from '../../components/Auth/LoginForm/LoginForm.jsx';
+
+
 const LoginPage = () => {
-  return <div className={css.wrapper}></div>;
+  return (
+    <div className={css.wrapper}>
+      <RegistrationFormTitle />
+      <LoginForm />
+    </div>
+  );
 };
+
 export default LoginPage;
