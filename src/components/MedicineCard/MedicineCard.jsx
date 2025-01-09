d8395ea1397e4632a4a6a9f5e30e7d2b583a68c8
Natalia-Kalashnikova <144282173+Natalia-Kalashnikova@users.noreply.github.com>
2025-01-10 01:31:28 +0200

    Add MedicineList on MedicinePage

--- src/components/MedicineCard/MedicineCard.jsx
+++ src/components/MedicineCard/MedicineCard.jsx
@@ -1,5 +1,50 @@
-const MedicineCard = () => {
-    return <div>MedicineCard</div>
+import { useMediaQuery } from "@mui/material";
+import ReactEllipsisText from "react-ellipsis-text";
+import { NavLink } from "react-router-dom";
+import css from './MedicineCard.module.css';
+
+const MedicineCard = ({product}) => {
+    const isTablet = useMediaQuery('(min-width: 768px) and (max-width: 1439px)');
+  const handleAddToCart = () => {
+    console.log(product);
+    };
+
+    return (
+            <>
+      <div className={css.imgWrapper}>
+        <img src={product.photo} alt={product.name} className={css.image} />
+      </div>
+      <div className={css.descriptionWrapper}>
+        <div className={css.textWrapper}>
+          {isTablet ? (
+            <ReactEllipsisText
+              className={css.title}
+              text={product.name}
+              length={12}
+            />
+          ) : (
+            <p className={css.title}>{product.name}</p>
+          )}
+          <p className={css.supplier}>{product.suppliers}</p>
+        </div>
+        <p className={css.price}>₴{product.price}</p>
+        <div className={css.btnWrapper}>
+          <button
+            type="button"
+            className={css.buttonAdd}
+            onClick={handleAddToCart}
+          >
+            Add to cart
+          </button>
+          <NavLink to={`/product/${product.id}`} className={css.buttonDetails}>
+            Details
+          </NavLink>
+        </div>
+      </div>
+    </>
+    )
+    
+
 }
 
 export default MedicineCard;
--- src/components/MedicineList/MedicineList.jsx
+++ src/components/MedicineList/MedicineList.jsx
@@ -1,5 +1,19 @@
+import fetchedProducts from '../../../products.json';
+import MedicineCard from '../../components/MedicineCard/MedicineCard.jsx';
+import css from './MedicineList.module.css';
+
 const MedicineList = () => {
-    return <div>MedicineList</div>
+    const products = fetchedProducts;
+
+    return (
+        <ul className={css.productList}>
+            {products.map(product => (
+                <li key={product.id} className={css.productItem}>
+                    <MedicineCard product={product} />
+                </li>
+            ))}
+        </ul>
+    );
 }
 
 export default MedicineList;
