1e8f80cecbbf9eddb29597579907733be9ade7b1
Natalia-Kalashnikova <144282173+Natalia-Kalashnikova@users.noreply.github.com>
2025-01-10 04:06:29 +0200

    Add Description and Reviews to the ProductPage

--- src/components/Description/Description.jsx
+++ src/components/Description/Description.jsx
@@ -1,5 +1,11 @@
-const Description = ({product}) => {
-    return <div>Description</div>
+import css from './Description.module.css';
+
+const Description = ({ product }) => {
+    return (
+    <div className={css.descriptionWrapper}>
+      <p className={css.text}>{product.description}</p>
+    </div>
+  );
 }
 
 export default Description;
--- src/components/TabsContainer/TabsContainer.jsx
+++ src/components/TabsContainer/TabsContainer.jsx
@@ -9,34 +9,29 @@ const TabsContainer = ({ product }) => {
     const handleTabClick = tabName => {
     setActiveTab(tabName);
   };
-  return (
-    <div className={css.container}>
-      <div className={css.tabsWrapper}>
-        <div className={css.tabs}>
-          <button
-            className={`${css.tabLink} ${
-              activeTab === 'Description' ? css.active : ''
-            }`}
-            onClick={() => handleTabClick('Description')}
-          >
-            Description
-          </button>
-          <button
-            className={`${css.tabLink} ${
-              activeTab === 'Reviews' ? css.active : ''
-            }`}
-            onClick={() => handleTabClick('Reviews')}
-          >
-            Reviews
-          </button>
-        </div>
-        <div className={css.divider}></div>
-      </div>
-      <div className={css.bottomWrapper}>
-        <div className={css.tabContent}>
-          {activeTab === 'Description' && <Description product={product} />}
-          {activeTab === 'Reviews' && <Reviews product={product} />}
-        </div>
+    return ( 
+    <div className={css.wrapper}>
+      <div className={css.tabsWrapper}>        
+        <button
+          className={`${css.tabLink} ${
+            activeTab === 'Description' ? css.active : ''
+          }`}
+          onClick={() => handleTabClick('Description')}
+        >
+          Description
+        </button>
+        <button
+          className={`${css.tabLink} ${
+            activeTab === 'Reviews' ? css.active : ''
+          }`}
+          onClick={() => handleTabClick('Reviews')}
+        >
+          Reviews
+        </button>
+      </div>      
+      <div className={css.tabContent}>
+        {activeTab === 'Description' && <Description product={product} />}
+        {activeTab === 'Reviews' && <Reviews product={product} />}
       </div>
     </div>
   );
